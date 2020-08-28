67fbe5d974a58553a551e751ab957fac1ff553d7
“talha0300” <talhaali0300@gmail.com>
2020-08-28 23:27:47 +0500

    Added Profile option for each user

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.7.0'
 gem 'devise'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
+gem 'pry', '~> 0.12.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  match 'users/:id' => 'users#show', via: :get
+  # or
+  get 'users/:id' => 'users#show'
+  get 'users' => 'users#index'
+  # or
+  resources :users, only: [:show]
+
   get 'home/index'
   root 'home#index'
 
